62f61ea812019a05b339c2f06eb26b2edb562011
eun101 <113440950+eun101@users.noreply.github.com>
2023-01-04 02:40:23 +0800

    Update AuthenticatedLayout.jsx

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -113,7 +113,7 @@ export default function Authenticated({ auth, header, children }) {
                             </NavLink>
                         </div>
                         <div>
-                            <NavLink href={route('product.index')} active={route().current('product.index')}>
+                            <NavLink href={route('books.index')} active={route().current('books.index')}>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="w-6 h-6"
@@ -130,7 +130,7 @@ export default function Authenticated({ auth, header, children }) {
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                                 </svg>
                                 <span className="">
-                                    Products
+                                    Books
                                 </span>
                             </NavLink>
                         </div>
