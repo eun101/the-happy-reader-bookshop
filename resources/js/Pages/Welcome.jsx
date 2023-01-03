59fa37875d555a42273bfb331a3ecd715c32d632
eun101 <113440950+eun101@users.noreply.github.com>
2023-01-04 02:47:50 +0800

    Update Welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -31,7 +31,6 @@ export default function Welcome(props) {
 
 
 
-
                         ) : (
                             <>
                                 <Link href={route('login')} className="text-sm text-gray-700 dark:text-gray-500 underline">
